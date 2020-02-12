f2c476cbd58fbdf633078ff9e4eebd23f9ae46a7
Jared Wolff <hello@jaredwolff.com>
2020-02-12 17:02:02 -0500

    Removing cruft

--- include/serial.h
+++ include/serial.h
@@ -4,19 +4,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// typedef uint32_t (*serial_begin_t)(uint32_t baud);
-// typedef int (*serial_available_t)();
-// typedef void (*serial_println_t)(char *data);
-// typedef char (*serial_println_t)();
-
-// typedef struct
-// {
-//   serial_begin_t begin;
-//   serial_available_t available;
-//   serial_println_t println;
-//   serial_read_t read;
-// };
-
 void serial_begin(uint32_t baud);
 int serial_available(void);
 size_t serial_println(const char *data);
