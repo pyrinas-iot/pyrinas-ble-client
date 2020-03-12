a7a6981de47254425e46f1332717d5f1aa3cbc18
Jared Wolff <hello@jaredwolff.com>
2020-03-12 19:38:57 -0400

    Needs to be 1000 for tick calculations

--- main/config/FreeRTOSConfig.h
+++ main/config/FreeRTOSConfig.h
@@ -59,7 +59,7 @@
 #define configUSE_TICKLESS_IDLE 1
 #define configUSE_TICKLESS_IDLE_SIMPLE_DEBUG 1 /* See into vPortSuppressTicksAndSleep source code for explanation */
 #define configCPU_CLOCK_HZ (SystemCoreClock)
-#define configTICK_RATE_HZ 1024
+#define configTICK_RATE_HZ 1000
 #define configMAX_PRIORITIES (3)
 #define configMINIMAL_STACK_SIZE (60)
 #define configTOTAL_HEAP_SIZE (4096)
