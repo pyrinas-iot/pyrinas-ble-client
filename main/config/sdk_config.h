364b027687eed5449958eb4197b14fb5927e0087
Jared Wolff <hello@jaredwolff.com>
2021-07-07 21:01:58 -0400

    Small tweaks.
    Changed:
    * Increase queue size

--- main/config/sdk_config.h
+++ main/config/sdk_config.h
@@ -978,7 +978,7 @@
 // <i> will fail.
 
 #ifndef APP_TIMER_CONFIG_OP_QUEUE_SIZE
-#define APP_TIMER_CONFIG_OP_QUEUE_SIZE 10
+#define APP_TIMER_CONFIG_OP_QUEUE_SIZE 16
 #endif
 
 // <q> APP_TIMER_CONFIG_USE_SCHEDULER  - Enable scheduling app_timer events to app_scheduler
